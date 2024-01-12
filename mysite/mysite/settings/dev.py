819269329a9ce24fdd815a602803120d6695db15
Raldo Gerald Chimwendo <114239029+Mr-Raldo@users.noreply.github.com>
2024-01-12 12:39:51 +0200

    wagtail

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ze$4m_7i=7b+#8+=4sj$13=oni#hkdn76mhupi@^o3^btq&1rf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
